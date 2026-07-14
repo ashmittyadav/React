0b28ae2af1c2d65d5de415f8c5def9d502cd6383
ashister <ashmityadavat92@gmail.com>
2026-07-14 21:39:08 +0530

    React Counter-app project

---
+++ Projects/Counter-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import Counter from './Counter'
+
+const App = () => {
+
+
+
+  return (
+    <div className='div'>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
