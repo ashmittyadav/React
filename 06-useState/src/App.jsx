62de510688df83fcdc128ef20e27e54ba2f38f14
ashister <ashmityadavat92@gmail.com>
2026-07-15 22:58:09 +0530

    Modified use-staten implementation

--- 06-useState/src/App.jsx
+++ 06-useState/src/App.jsx
@@ -5,17 +5,21 @@ const App = () => {
 
   const [num, setNum] = useState(21)
   const [userName, setUserName] = useState('Ashmit')
+  const [obj, setobj] = useState({user:'bhaukal', age:19})
 
-  function changeNum() {
+
+  const changeNum = () => {
     console.log('State changed');
     setNum(20)
-    setUserName('Kritika')
+    setUserName('Aditi')
+    setobj(prev=>({...prev,age:30}))
     
   }
 
   return (
     <div className='div'>
       <h1>My name : {userName} <br/> My age : {num}</h1>
+      <h1>{obj.user} <br/> {obj.age}</h1>
       <button onClick={changeNum} className='btn'>Click</button>
       <Counter />
     </div>
